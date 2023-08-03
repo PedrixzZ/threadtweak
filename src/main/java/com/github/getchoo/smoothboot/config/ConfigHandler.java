9fdd5efc9dbbaa5bb947546069bd90126231f6c9
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-08-03 01:12:35 -0300

    Update ConfigHandler.java

--- src/main/java/com/github/getchoo/smoothboot/config/ConfigHandler.java
+++ src/main/java/com/github/getchoo/smoothboot/config/ConfigHandler.java
@@ -14,39 +14,48 @@ import java.io.IOException;
 public class ConfigHandler {
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
-    public static SmoothBootConfig readConfig() throws IOException {
+    private static boolean configChanged = false;
+
+    public static SmoothBootConfig readConfig() {
         String configPath = System.getProperty("user.dir") + "/config/" + SmoothBoot.MOD_ID + ".json";
         SmoothBoot.LOGGER.debug("Config path: " + configPath);
 
-        // Read config
         SmoothBootConfig config;
         try (FileReader reader = new FileReader(configPath)) {
             config = GSON.fromJson(reader, SmoothBootConfig.class);
             if (config == null) {
-                throw new NullPointerException();
+                throw new JsonParseException("Invalid config file format");
             }
             config.validate();
-            try (FileWriter writer = new FileWriter(configPath)) {
-                GSON.toJson(config, writer);
-            }
-
             SmoothBoot.LOGGER.debug("Config: " + config);
-        } catch (NullPointerException | JsonParseException | IOException e) {
-            // Create new config
+        } catch (IOException | JsonParseException e) {
+            SmoothBoot.LOGGER.error("Error reading config: " + e.getMessage());
+            // Create new default config
             config = new SmoothBootConfig();
-            File file = new File(configPath);
-            file.getParentFile().mkdirs();
-            try (FileWriter writer = new FileWriter(configPath)) {
-                GSON.toJson(config, writer);
-                SmoothBoot.LOGGER.debug("New config file created");
-            }
+            configChanged = true;
         }
+
         return config;
+    }
+
+    public static void saveConfig(SmoothBootConfig config) {
+        String configPath = System.getProperty("user.dir") + "/config/" + SmoothBoot.MOD_ID + ".json";
+
+        try (FileWriter writer = new FileWriter(configPath)) {
+            GSON.toJson(config, writer);
+            configChanged = false;
+            SmoothBoot.LOGGER.debug("Config file saved");
+        } catch (IOException e) {
+            SmoothBoot.LOGGER.error("Error saving config: " + e.getMessage());
+        }
     }
 
     public static void openConfigFile() {
         String configPath = System.getProperty("user.dir") + "/config/" + SmoothBoot.MOD_ID + ".json";
-
         Util.getOperatingSystem().open(new File(configPath));
+    }
+
+    public static boolean hasConfigChanged() {
+        return configChanged;
     }
 }
