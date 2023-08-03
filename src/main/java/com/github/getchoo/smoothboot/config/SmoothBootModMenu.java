aab16d0e971c51da5aeb9efe76277e7a2b91d04c
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-08-03 01:15:44 -0300

    Update SmoothBootModMenu.java

--- src/main/java/com/github/getchoo/smoothboot/config/SmoothBootModMenu.java
+++ src/main/java/com/github/getchoo/smoothboot/config/SmoothBootModMenu.java
@@ -3,16 +3,26 @@ package com.github.getchoo.smoothboot.config;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 
 public class SmoothBootModMenu implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return screen -> new Screen(Text.of("")) {
+        return parent -> new ConfigScreen(Text.of("SmoothBoot Config")) {
             @Override
             protected void init() {
-                ConfigHandler.openConfigFile();
-                this.client.setScreen(screen);
+                super.init();
+                this.addDrawableChild(new ButtonWidget(this.width / 2 - 100, this.height / 2, 200, 20, Text.of("Open Config File"), (button) -> {
+                    ConfigHandler.openConfigFile();
+                }));
+            }
+
+            @Override
+            public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+                this.renderBackground(matrices);
+                super.render(matrices, mouseX, mouseY, delta);
             }
         };
     }
