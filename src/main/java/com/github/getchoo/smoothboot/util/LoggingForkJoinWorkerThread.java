d7141772c52d980f75e8bf38bcff78057607228b
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-08-03 01:11:27 -0300

    Update LoggingForkJoinWorkerThread.java

--- src/main/java/com/github/getchoo/smoothboot/util/LoggingForkJoinWorkerThread.java
+++ src/main/java/com/github/getchoo/smoothboot/util/LoggingForkJoinWorkerThread.java
@@ -6,21 +6,21 @@ import java.util.concurrent.ForkJoinWorkerThread;
 import org.apache.logging.log4j.Logger;
 
 public class LoggingForkJoinWorkerThread extends ForkJoinWorkerThread {
-	private final Logger logger;
+    private final Logger logger;
 
-	public LoggingForkJoinWorkerThread(ForkJoinPool pool, Logger logger) {
-		super(pool);
-		this.logger = logger;
-	}
+    public LoggingForkJoinWorkerThread(ForkJoinPool pool, Logger logger) {
+        super(pool);
+        this.logger = logger;
+    }
 
-	@Override
-	protected void onTermination(Throwable throwable) {
-		if (throwable != null) {
-			logger.warn("{} died", this.getName(), throwable);
-		} else {
-			logger.debug("{} shutdown", this.getName());
-		}
+    @Override
+    protected void onTermination(Throwable throwable) {
+        if (throwable != null) {
+            logger.warn("{} died", this.getName(), throwable);
+        } else {
+            logger.debug("{} shutdown", this.getName());
+        }
 
-		super.onTermination(throwable);
-	}
+        super.onTermination(throwable);
+    }
 }
