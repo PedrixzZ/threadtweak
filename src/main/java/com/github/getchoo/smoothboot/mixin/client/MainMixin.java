993de5f94888d7c8aae6cd300ecd64a6085665e0
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-08-03 01:09:12 -0300

    Update MainMixin.java

--- src/main/java/com/github/getchoo/smoothboot/mixin/client/MainMixin.java
+++ src/main/java/com/github/getchoo/smoothboot/mixin/client/MainMixin.java
@@ -10,14 +10,16 @@ import net.minecraft.client.main.Main;
 
 @Mixin(Main.class)
 public class MainMixin {
-	@Inject(method = "main", at = @At("HEAD"), remap = false)
-	private static void onMain(CallbackInfo ci) {
-		if (!SmoothBoot.initConfig) {
-			SmoothBoot.regConfig();
-			SmoothBoot.initConfig = true;
-		}
-		
-		Thread.currentThread().setPriority(SmoothBoot.config.threadPriority.game);
-		SmoothBoot.LOGGER.debug("Initialized client game thread");
-	}
+    private static boolean initialized = false;
+
+    @Inject(method = "main", at = @At("HEAD"), remap = false)
+    private static void onMain(CallbackInfo ci) {
+        if (!initialized) {
+            SmoothBoot.regConfig();
+            initialized = true;
+        }
+
+        Thread.currentThread().setPriority(SmoothBoot.config.threadPriority.game);
+        SmoothBoot.LOGGER.debug("Initialized client game thread");
+    }
 }
